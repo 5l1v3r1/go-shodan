7c57354320beec293e75b1f986adbebb29453668
obehi.iredia@gmail.com <obehi.iredia@gmail.com>
2020-04-23 22:18:46 -0500

    Add exploit API

--- shodan/shodan.go
+++ shodan/shodan.go
@@ -3,6 +3,9 @@ package shodan
 // BaseURL defines the Shodan URL
 const BaseURL = "https://api.shodan.io"
 
+// ExploitURL for finding multiple exploits on Shodan
+const ExploitURL = "https://exploits.shodan.io"
+
 // Client holds the API key
 type Client struct {
 	apiKey string
