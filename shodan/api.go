124337f081958c4db6282f9dd7b576fd4e445832
obehi.iredia@gmail.com <obehi.iredia@gmail.com>
2020-04-26 23:01:58 -0400

    Fix Shodan package & Exploit to main

--- shodan/api.go
+++ shodan/api.go
@@ -1,4 +1,4 @@
-package api
+package shodan
 
 import (
 	"encoding/json"
@@ -6,23 +6,24 @@ import (
 	"net/http"
 )
 
-// Profile struct to hold Shodan account information
-type Profile struct {
-	Member       bool    `json: "member"`
-	Credits      int     `json: "credits"`
-	Display_name string  `json: "display_name"`
-	Created      float64 `json: "created"`
+type ProfileInfo struct {
+	QueryCredits int    `json:"query_credits"`
+	ScanCredits  int    `json:"scan_credits"`
+	Telnet       bool   `json:"telnet"`
+	Plan         string `json:"plan"`
+	Https        bool   `json:"https"`
+	Unlocked     bool   `json:"unlocked"`
 }
 
-func (s *Client) Profile() (*Profile, error) {
-	req, err := http.Get(fmt.Sprintf("%s/api-info?key=%s", BaseURL, s.apiKey))
+func (s *Client) ProfileInfo() (*ProfileInfo, error) {
+	res, err := http.Get(fmt.Sprintf(BaseURL+"/api-info?key=%s", s.apiKey))
 	if err != nil {
 		return nil, err
 	}
-	defer req.Body.Close()
+	defer res.Body.Close()
 
-	var ret Profile
-	if err := json.NewDecoder(req.Body).Decode(&ret); err != nil {
+	var ret ProfileInfo
+	if err := json.NewDecoder(res.Body).Decode(&ret); err != nil {
 		return nil, err
 	}
 	return &ret, nil
--- shodan/host.go
+++ shodan/host.go
@@ -1,4 +1,4 @@
-package host
+package shodan
 
 import (
 	"encoding/json"
@@ -6,50 +6,51 @@ import (
 	"net/http"
 )
 
-// HostInfo struct is responsible for returned data in Matches
-type HostInfo struct {
-	Os        string   `json: "os"`
-	Timestamp string   `json: "timestamp"`
-	Isp       string   `json: "isp"`
-	Asn       string   `json: "asn"`
-	Hostnames []string `json: "hostnames"`
-	IP        string   `json: "ip"`
-	Domains   []string `json: "domains"`
-	Org       string   `json: "org"`
-	Data      string   `json: "data"`
-	Port      int      `json: "port"`
-	Ip_string string   `json: "ip_string"`
-}
-
-// HostLocation is responsible for returned data in a single element
 type HostLocation struct {
-	City          string  `json: "city"`
-	Region_code   int     `json: "region_code"`
-	Area_code     int     `json: "area_code"`
-	Longitude     float64 `json: "longitude"`
-	Country_code3 string  `json: "country_code3"`
-	Country_name  string  `json: "country_name"`
-	Postal_code   int     `json: "postal_code"`
-	Dma_code      int     `json: "dma_code"`
-	Country_code  string  `json: "country_code"`
-	Latitude      int     `json: "latitude"`
+	City         string  `json:"city"`
+	RegionCode   string  `json:"region_code"`
+	AreaCode     int     `json:"area_code"`
+	Longitude    float32 `json:"longitude"`
+	CountryCode3 string  `json:"country_code3"`
+	CountryName  string  `json:"country_name"`
+	PostalCode   string  `json:"postal_code"`
+	DMACode      int     `json:"dma_code"`
+	CountryCode  string  `json:"country_code"`
+	Latitude     float32 `json:"latitude"`
 }
 
-// HostSearch is responsible for parsing data from the response
-type HostSearch struct {
-	Matches []HostInfo `json: "matches"`
+type Host struct {
+	OS        string       `json:"os"`
+	Timestamp string       `json:"timestamp"`
+	ISP       string       `json:"isp"`
+	ASN       string       `json:"asn"`
+	Hostnames []string     `json:"hostnames"`
+	Location  HostLocation `json:"location"`
+	IP        int64        `json:"ip"`
+	Domains   []string     `json:"domains"`
+	Org       string       `json:"org"`
+	Data      string       `json:"data"`
+	Port      int          `json:"port"`
+	IPString  string       `json:"ip_str"`
 }
 
-func (s *Client) HostSearch(q string) (*HostSearch, error) {
-	res, err := http.Get(fmt.Sprintf("%s/shodan/host/search?key=%s&query=%s", BaseURL, s.apiKey, q))
+type ProfileSearch struct {
+	Matches []Host `json:"matches"`
+}
+
+func (s *Client) ProfileSearch(q string) (*ProfileSearch, error) {
+	res, err := http.Get(
+		fmt.Sprintf(BaseURL+"/shodan/host/search?key=%s&query=%s", s.apiKey, q),
+	)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 	defer res.Body.Close()
 
-	var ret HostSearch
+	var ret ProfileSearch
 	if err := json.NewDecoder(res.Body).Decode(&ret); err != nil {
 		return nil, err
 	}
+
 	return &ret, nil
 }
